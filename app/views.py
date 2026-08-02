5214b4209b70ad448b411ca27c503a59fff91bab
Abbas Hilal <168707838+Abbas463@users.noreply.github.com>
2026-08-02 16:53:42 +0430

    Implement student registration form with index view and HTML template

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        print('name is', name)
+    return render(request, 'index.html')
