1b050124f7fce2cb239d47d9c5d4106a752722f0
Abbas Hilal <168707838+Abbas463@users.noreply.github.com>
2026-08-02 18:26:59 +0430

    Add Student model and registration form validation

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models, Model):
+    name=models.CharField(max_length=100)
+    emial=models.EmailField()
+    dob=models.DateField()
+    gen=models.CharField(max_length=50)
+    stuClass=models.TextField()
+    reg=models.TextField()
+    test=models.FloatField()
--- app/views.py
+++ app/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Student
 
 # Create your views here.
 def index(request):
     if request.method=='POST':
         name=request.POST['name']
-        print('name is', name)
+        email=request.POST['email']
+        dob=request.POST['dob']
+        gen=request.POST['gender']
+        cla=request.POST['class']
+        regn=request.POST['reg']
+        test=request.POST['test']
+
+        if test > 100:
+            return HttpResponse("Test Scores must be less than 100")
+
+        print('name is', name, email, dob, gen, cla, regn, test)
         return render(request, 'index.html')
 
     return render(request, 'index.html')
