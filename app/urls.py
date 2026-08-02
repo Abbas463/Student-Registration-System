e5af56b925945c864a3062e6f2b7674242bfc3b2
Abbas Hilal <168707838+Abbas463@users.noreply.github.com>
2026-08-02 19:58:33 +0430

    Refactor admin registration for Student model, enhance index.html structure, add success and list templates, and implement list view for students

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
+    path('list/', views.list_stu, name="list"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,29 +1,34 @@
 from django.shortcuts import render, HttpResponse
 from .models import Student
 
-# Create your views here.
 def index(request):
-    if request.method=='POST':
-        name=request.POST['name']
-        email=request.POST['email']
-        dob=request.POST['dob']
-        gen=request.POST['gender']
-        cla=request.POST['class']
-        regn=request.POST['reg']
-        test=request.POST['test']
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        dob = request.POST['dob']
+        gen = request.POST['gender']
+        cla = request.POST['class']
+        regn = request.POST['reg']
+        test = request.POST['test']
 
         if float(test) > 100:
             return HttpResponse("Test Scores must be less than 100")
-        obj=Student()
-        obj.name=name
-        obj.email=email
-        obj.dob=dob
-        obj.gen=gen
-        obj.stuClass=cla
-        obj.reg=regn
-        obj.test=test
+
+        obj = Student()
+        obj.name = name
+        obj.email = email
+        obj.dob = dob
+        obj.gen = gen
+        obj.stuClass = cla
+        obj.reg = regn
+        obj.test = test
         obj.save()
 
-        return HttpResponse("<h1>Data Stored Successfully</h1>")
+        return render(request, "success.html")
 
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
+
+def list_stu(request):
+    obj = Student.objects.all()
+    return render(request, "list.html", {"obj": obj})
